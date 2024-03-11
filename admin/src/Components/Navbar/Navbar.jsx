3f07c8542ab50eb43afaf29a18e6777366d032eb
Utkarsh More <utkarsh.more.2000@gmail.com>
2024-03-11 11:48:57 -0400

    final changes

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../Assets/nav-logo.svg'
+import logo from '../Assets/logo.png'
 import navprofileIcon from '../Assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src={navlogo} className='nav-logo' alt="" />
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>Luxecart</p>
+      </div>
       <img src={navprofileIcon} className='nav-profile' alt="" />
     </div>
   )
